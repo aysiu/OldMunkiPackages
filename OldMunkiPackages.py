7f9c5f92bb34f3da948ca32adc28573b817dd075
A.Y. Siu <aysiu@users.noreply.github.com>
2016-03-11 11:17:53 -0800

    No need to write the plist. Let the user do it.

--- OldMunkiPackages.py
+++ OldMunkiPackages.py
@@ -27,9 +27,8 @@ if os.path.exists(omp_prefs_location):
 	omp_prefs=plistlib.readPlist(omp_prefs_location)
 	where_to_dump=omp_prefs['dump_location']
 else:
-	where_to_dump=default_where_to_dump
 	print "Cannot determine a dump location from %s. Will be dumping to %s." % (omp_prefs_location, where_to_dump)
-	plistlib.writePlist({dump_location: default_where_to_dump}, omp_prefs)
+	where_to_dump=default_where_to_dump
 
 # Where is make catalogs?
 makecatalogs='/usr/local/munki/makecatalogs'
