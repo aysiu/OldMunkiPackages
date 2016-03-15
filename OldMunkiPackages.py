3c4dcd6e3b96b3121c2ab0e9815a50d97bd64f8f
A.Y. Siu <aysiu@users.noreply.github.com>
2016-03-15 09:42:24 -0700

    Main to own function

--- OldMunkiPackages.py
+++ OldMunkiPackages.py
@@ -81,113 +81,119 @@ def check_folder_writable(checkfolder):
 		logging.error("You don't have access to %s" % checkfolder)
 		sys.exit(1)
 
-# Try to get the new Munki path
-munkiimport_prefs_location=os.path.join(os.getenv("HOME"), "Library/Preferences/com.googlecode.munki.munkiimport.plist")
-if os.path.exists(munkiimport_prefs_location):
-	munkiimport_prefs=plistlib.readPlist(munkiimport_prefs_location)
-	MUNKI_ROOT_PATH=munkiimport_prefs['repo_path']
-else:
-	logging.error("Cannot find the %s preferences file to read the Munki repo path" % munkiimport_prefs_location)
-	sys.exit(1)
+# Main
+def main():
 
-# Where should old packages be moved to? User Trash by default
-default_where_to_dump=os.path.join(os.getenv("HOME"), ".Trash")
-omp_prefs_location=os.path.join(os.getenv("HOME"), "Library/Preferences/com.github.aysiu.omp.plist")
-if os.path.exists(omp_prefs_location):
-	omp_prefs=plistlib.readPlist(omp_prefs_location)
-	if os.path.exists(omp_prefs['dump_location']):
-		where_to_dump=omp_prefs['dump_location']
-		logging.info("Will use dump location from the preferences file of %s." % where_to_dump)
+	# Try to get the new Munki path
+	munkiimport_prefs_location=os.path.join(os.getenv("HOME"), "Library/Preferences/com.googlecode.munki.munkiimport.plist")
+	if os.path.exists(munkiimport_prefs_location):
+		munkiimport_prefs=plistlib.readPlist(munkiimport_prefs_location)
+		MUNKI_ROOT_PATH=munkiimport_prefs['repo_path']
+	else:
+		logging.error("Cannot find the %s preferences file to read the Munki repo path" % munkiimport_prefs_location)
+		sys.exit(1)
+
+	# Where should old packages be moved to? User Trash by default
+	default_where_to_dump=os.path.join(os.getenv("HOME"), ".Trash")
+	omp_prefs_location=os.path.join(os.getenv("HOME"), "Library/Preferences/com.github.aysiu.omp.plist")
+	if os.path.exists(omp_prefs_location):
+		omp_prefs=plistlib.readPlist(omp_prefs_location)
+		if os.path.exists(omp_prefs['dump_location']):
+			where_to_dump=omp_prefs['dump_location']
+			logging.info("Will use dump location from the preferences file of %s." % where_to_dump)
+		else:
+			where_to_dump=default_where_to_dump
+			logging.info("Cannot determine a dump location from %s. Will be dumping to %s." % (omp_prefs_location, where_to_dump))
 	else:
 		where_to_dump=default_where_to_dump
 		logging.info("Cannot determine a dump location from %s. Will be dumping to %s." % (omp_prefs_location, where_to_dump))
-else:
-	where_to_dump=default_where_to_dump
-	logging.info("Cannot determine a dump location from %s. Will be dumping to %s." % (omp_prefs_location, where_to_dump))
 
-# Where is make catalogs?
-makecatalogs='/usr/local/munki/makecatalogs'
+	# Where is make catalogs?
+	makecatalogs='/usr/local/munki/makecatalogs'
 
-MUNKI_PKGS_DIR_NAME = 'pkgs'
-MUNKI_PKGSINFO_DIR_NAME = 'pkgsinfo'
+	MUNKI_PKGS_DIR_NAME = 'pkgs'
+	MUNKI_PKGSINFO_DIR_NAME = 'pkgsinfo'
 
-# Join paths based on what's defined
-pkgsinfo_path=os.path.join(MUNKI_ROOT_PATH, MUNKI_PKGSINFO_DIR_NAME)
-pkgs_path=os.path.join(MUNKI_ROOT_PATH, MUNKI_PKGS_DIR_NAME)
+	# Join paths based on what's defined
+	pkgsinfo_path=os.path.join(MUNKI_ROOT_PATH, MUNKI_PKGSINFO_DIR_NAME)
+	pkgs_path=os.path.join(MUNKI_ROOT_PATH, MUNKI_PKGS_DIR_NAME)
 
-# Check that the paths for the pkgsinfo and pkgs exist
-if not os.path.isdir(pkgsinfo_path) and not os.path.isdir(pkgs_path):
-	logging.error("Your pkgsinfo and pkgs paths are not valid. Please check your MUNKI_ROOT_PATH value")
-else:
-	# Make sure all relevant folders are writable
-	check_folder_writable(pkgsinfo_path)
-	check_folder_writable(pkgs_path)
-	check_folder_writable(where_to_dump)
-	
-	# A list to store all items
-	all_items = {};
-
-	# Lists to store items to delete
-	pkgs_to_delete = [];
-	pkgsinfo_to_delete = [];
-
-	# Walk through the pkgsinfo files...
-	for root, dirs, files in os.walk(pkgsinfo_path):
-		for dir in dirs:
-			# Skip directories starting with a period
-			if dir.startswith("."):
-				dirs.remove(dir)
-		for file in files:
-			# Skip files that start with a period
-			if file.startswith("."):
-				continue
-			fullfile = os.path.join(root, file)
-			plist = plistlib.readPlist(fullfile)
-			plistname = plist['name']
-			plistversion = plist['version']
-			plistcatalogs = plist['catalogs']
-			plistcatalogs.sort()
-			# Some items won't have an installer_item_location: nopkg .plist files, for example... that's okay
-			if 'installer_item_location' in plist:
-				plistinstaller_item_location = os.path.join(pkgs_path, plist['installer_item_location'])
-			else:
-				plistinstaller_item_location = ''
-		
-			# Create a dictionary based on the plist values read
-			plistdict={ 'pkginfo': fullfile, 'version': plistversion, 'catalogs': plistcatalogs, 'installer_item_location': plistinstaller_item_location}
-				
-			# See if the plist name is already in all_items
-			if plistname in all_items:
-				# Compare the previously existing one to the currently focused one to see if they have the same catalogs (fix this because it could be testing production or production testing)
-				if all_items[plistname]['catalogs'] == plistcatalogs:
-					# See if this is a newer version than the one in there
-					if cmp (MunkiLooseVersion(plistversion), MunkiLooseVersion(all_items[plistname]['version'])) > 0 :
-						# If this is newer, then move the old one to the items to delete list
-						if( all_items[plistname]['installer_item_location'] != '' ):
-							pkgs_to_delete.append(all_items[plistname]['installer_item_location'])
-						pkgsinfo_to_delete.append(all_items[plistname]['pkginfo'])
-						del all_items[plistname]
-						all_items[plistname]=plistdict
-					else:
-						# Otherwise, if this is older, keep the old one in there, and move this one to the delete list
-						if( plistdict['installer_item_location'] != '' ):
-							pkgs_to_delete.append(plistdict['installer_item_location'])
-						pkgsinfo_to_delete.append(plistdict['pkginfo'])
-			else:
-				# If it's not in the list already, add it
-				all_items[plistname]=plistdict		
-
-	if pkgs_to_delete:
-		trash_old_stuff(pkgs_to_delete, pkgs_path, where_to_dump)
-	if pkgsinfo_to_delete:
-		trash_old_stuff(pkgsinfo_to_delete, pkgsinfo_path, where_to_dump)
-
-	if pkgs_to_delete or pkgsinfo_to_delete:
-		# If /usr/local/munki/makecatalogs exists (it should), then run it to reflect the changes or let the user know to run it
-		if os.path.exists(makecatalogs):
-			logging.info("Running makecatalogs")
-			os.system(makecatalogs)
-		else:
-			logging.error("%s could not be found. When you have a chance, run makecatalogs on your Munki repo to have the changes reflected." % makecatalogs)
+	# Check that the paths for the pkgsinfo and pkgs exist
+	if not os.path.isdir(pkgsinfo_path) and not os.path.isdir(pkgs_path):
+		logging.error("Your pkgsinfo and pkgs paths are not valid. Please check your MUNKI_ROOT_PATH value")
 	else:
-		logging.info("Nothing old to dump.")
+		# Make sure all relevant folders are writable
+		check_folder_writable(pkgsinfo_path)
+		check_folder_writable(pkgs_path)
+		check_folder_writable(where_to_dump)
+	
+		# A list to store all items
+		all_items = {};
+
+		# Lists to store items to delete
+		pkgs_to_delete = [];
+		pkgsinfo_to_delete = [];
+
+		# Walk through the pkgsinfo files...
+		for root, dirs, files in os.walk(pkgsinfo_path):
+			for dir in dirs:
+				# Skip directories starting with a period
+				if dir.startswith("."):
+					dirs.remove(dir)
+			for file in files:
+				# Skip files that start with a period
+				if file.startswith("."):
+					continue
+				fullfile = os.path.join(root, file)
+				plist = plistlib.readPlist(fullfile)
+				plistname = plist['name']
+				plistversion = plist['version']
+				plistcatalogs = plist['catalogs']
+				plistcatalogs.sort()
+				# Some items won't have an installer_item_location: nopkg .plist files, for example... that's okay
+				if 'installer_item_location' in plist:
+					plistinstaller_item_location = os.path.join(pkgs_path, plist['installer_item_location'])
+				else:
+					plistinstaller_item_location = ''
+		
+				# Create a dictionary based on the plist values read
+				plistdict={ 'pkginfo': fullfile, 'version': plistversion, 'catalogs': plistcatalogs, 'installer_item_location': plistinstaller_item_location}
+				
+				# See if the plist name is already in all_items
+				if plistname in all_items:
+					# Compare the previously existing one to the currently focused one to see if they have the same catalogs (fix this because it could be testing production or production testing)
+					if all_items[plistname]['catalogs'] == plistcatalogs:
+						# See if this is a newer version than the one in there
+						if cmp (MunkiLooseVersion(plistversion), MunkiLooseVersion(all_items[plistname]['version'])) > 0 :
+							# If this is newer, then move the old one to the items to delete list
+							if( all_items[plistname]['installer_item_location'] != '' ):
+								pkgs_to_delete.append(all_items[plistname]['installer_item_location'])
+							pkgsinfo_to_delete.append(all_items[plistname]['pkginfo'])
+							del all_items[plistname]
+							all_items[plistname]=plistdict
+						else:
+							# Otherwise, if this is older, keep the old one in there, and move this one to the delete list
+							if( plistdict['installer_item_location'] != '' ):
+								pkgs_to_delete.append(plistdict['installer_item_location'])
+							pkgsinfo_to_delete.append(plistdict['pkginfo'])
+				else:
+					# If it's not in the list already, add it
+					all_items[plistname]=plistdict		
+
+		if pkgs_to_delete:
+			trash_old_stuff(pkgs_to_delete, pkgs_path, where_to_dump)
+		if pkgsinfo_to_delete:
+			trash_old_stuff(pkgsinfo_to_delete, pkgsinfo_path, where_to_dump)
+
+		if pkgs_to_delete or pkgsinfo_to_delete:
+			# If /usr/local/munki/makecatalogs exists (it should), then run it to reflect the changes or let the user know to run it
+			if os.path.exists(makecatalogs):
+				logging.info("Running makecatalogs")
+				os.system(makecatalogs)
+			else:
+				logging.error("%s could not be found. When you have a chance, run makecatalogs on your Munki repo to have the changes reflected." % makecatalogs)
+		else:
+			logging.info("Nothing old to dump.")
+
+if __name__ == '__main__':
+    main()
